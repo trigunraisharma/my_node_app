86e851e7ac3bc7aaf54225e098b12af065ad1fb2
trigunraisharma <trigunraisharma@gmail.com>
2025-10-23 21:20:49 +0530

    Initial commit

---
+++ my_node_app/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://mongo:27017/mydb")
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node + Mongo!");
+});
+
+app.listen(3000, () => console.log("🚀 App running on port 3000"));
+
